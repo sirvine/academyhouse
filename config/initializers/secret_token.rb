511dee36bf5d011162b0e131d65e39480aecb070
Sol Irvine <sol@densevector.com>
2013-07-15 23:13:54 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Academyhouse::Application.config.secret_token = 'ff66e648620ffea377e67f4e5234847b6d17f46dd5827c9a294ea8b9d0874930915e6b467ed557980c1e39669236d0d2fcb0531419a624366a52597ca8ac0c96'
